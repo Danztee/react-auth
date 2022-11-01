3fa78b59874f65e898f6764521af92d34a3b975a
Danztee <olowoniyidaniel@gmail.com>
2022-11-01 21:31:15 +0100

    auth

---
+++ src/api-key.js
@@ -0,0 +1,1 @@
+export const API_KEY = "AIzaSyDj0PK6b8E1iwFp3uocjtdvNzsYg7d3wLU";
---
+++ src/components/Profile/ProfileForm.js
@@ -0,0 +1,55 @@
+import { useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_KEY } from "../../api-key";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
+
+const ProfileForm = () => {
+  const authCtx = useContext(AuthContext);
+  const { token } = authCtx;
+  const newPasswordInputRef = useRef();
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      console.log(res);
+      navigate("/");
+    });
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;
